19afface60d1aee45788e24a806aa22ee5b11a86
Federico Aloi <federico.aloi@gmail.com>
2016-07-04 18:28:23 -0300

    "Test" de integracion para hacer pruebas con instancias y solvers

--- lib/knapsack/instance.rb
+++ lib/knapsack/instance.rb
@@ -13,7 +13,17 @@ class Knapsack::Instance
     self.new test_code, to_items(lines.drop(1).clip), lines.last.to_i, read_optimal_value(test_code)
   end
 
+  def self.all
+    prefix_length = 5
+    suffix_length = 3
+    Dir.entries(make_path).select {|x| x.end_with? 'in'}.map {|x| from_file x[prefix_length...x.length - suffix_length] }
+  end
+
   private
+
+  def self.make_path(subdir = '')
+    "tests/#{subdir}"
+  end
 
   def self.to_items(item_lines)
     item_lines.map do |line|
@@ -23,11 +33,11 @@ class Knapsack::Instance
   end
 
   def self.read_instance_file(test_code)
-    File.readlines "tests/test_#{test_code}.in"
+    File.readlines (make_path "test_#{test_code}.in")
   end
 
   def self.read_optimal_value(test_code)
     position = test_code[0...3].to_i
-    File.readlines('tests/opt_values.txt')[position].to_i
+    File.readlines(make_path 'opt_values.txt')[position].to_i
   end
 end
---
+++ spec/integration_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'Integration spec' do
+  let(:instances) { Knapsack::Instance.all }
+  let(:solvers) { [Knapsack::Solvers::Greedy.new] }
+
+  context 'comparative' do
+    let(:solutions) { instances.map {|i| [i, solvers.map {|s| s.solve_for i }] } }
+    before { puts solutions }
+    it { expect(1).to eq(1) }
+  end
+end
