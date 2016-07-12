528e33f497ed7d5973ea3d61dd71596665e7f17e
Federico Aloi <federico.aloi@gmail.com>
2016-07-11 23:42:26 -0300

    El Greedy puede computar los limites para el B&B

--- lib/knapsack/solvers/greedy.rb
+++ lib/knapsack/solvers/greedy.rb
@@ -2,12 +2,36 @@ require_relative './base'
 
 class Knapsack::Solvers::Greedy < Knapsack::Solvers::Base
   def compute_solution_for(instance)
+    compute_for(instance).first
+  end
+
+  def compute_bounds_for(instance)
+    greedy, (next_item, *), remaining_capacity = compute_for instance
+
+    lower_bound = greedy.sum(&:gain)
+    relaxed_problem_plus = remaining_capacity > 0 ? next_item.rate * remaining_capacity : 0
+
+    [lower_bound, lower_bound + relaxed_problem_plus]
+  end
+
+  def compute_for(instance)
     remaining_capacity = instance.capacity
     sorted_by_rate = instance.items.sort_by(&:rate).reverse
 
-    sorted_by_rate.take_while do |x|
-      remaining_capacity -= x.weight
-      remaining_capacity >= 0
+    result = []
+    next_index = 0
+    while remaining_capacity >= 0 && next_index < sorted_by_rate.size
+      next_item = sorted_by_rate[next_index]
+
+      if next_item.weight <= remaining_capacity
+        result << next_item
+        remaining_capacity -= next_item.weight
+        sorted_by_rate.delete_at next_index
+      else
+        next_index += 1
+      end
     end
+
+    [result, sorted_by_rate, remaining_capacity]
   end
 end
--- spec/greedy_solver_spec.rb
+++ spec/greedy_solver_spec.rb
@@ -1,13 +1,19 @@
 require 'spec_helper'
 
 describe Knapsack::Solvers::Greedy do
-  describe '#solve_for' do
-    let(:instance) { Knapsack::Instance.from_file '000_3e1' }
+  let(:instance) { Knapsack::Instance.from_file '000_3e1' }
+  let(:greedy) { Knapsack::Solvers::Greedy.new }
 
-    context 'works' do
-      let(:solution) { Knapsack::Solvers::Greedy.new.solve_for instance }
-      before { puts solution }
-      it { expect(solution.value).to eq 348 }
-    end
+  describe '#solve_for' do
+    let(:solution) { greedy.solve_for instance }
+    before { puts solution }
+    it { expect(solution.value).to be <= instance.optimal_value }
+  end
+
+  describe '#compute_bounds_for'do
+    let(:bounds) { greedy.compute_bounds_for instance }
+    before { puts bounds.to_s }
+    it { expect(bounds.first).to be <= instance.optimal_value }
+    it { expect(bounds.last).to be >= instance.optimal_value }
   end
 end
